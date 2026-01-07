8d856e526e68cd3abd458b723623f23b20ed0bdb
xjcrazy <ryan@villarreal.com>
2026-01-07 18:33:35 -0500

    initial commit

---
+++ default.nix
@@ -0,0 +1,43 @@
+{ pkgs, ... }:
+
+{
+  # first always
+  home.stateVersion = "26.05";
+
+  # personal preference
+  nixpkgs.config = {
+    allowUnfree = true;
+    allowBroken = true;
+  };
+
+  home.username = "null";
+  home.homeDirectory = "/home/null";
+
+  # programs installed here
+  # but modified in ./modules/
+  home.packages = with pkgs; [
+    # sorta system stuff
+    nerd-fonts.jetbrains-mono
+    catppuccin
+
+    kdePackages.fcitx5-with-addons
+    # system administration apps
+    docker-compose
+    screen
+    unzip
+    openssl
+    usbutils
+    btop
+    htop
+
+    # programming
+    go
+    wezterm
+    wofi
+    alacritty
+  ];
+
+  # Let Home Manager install and manage itself.
+  programs.home-manager.enable = true;
+
+}
